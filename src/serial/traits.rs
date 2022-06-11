3908befec2cde32d79ddd9ebde9288d6f6facb11
KMConner <KMConner@users.noreply.github.com>
2022-06-11 14:18:01 +0100

    Add readwrite trait

--- src/serial/traits.rs
+++ src/serial/traits.rs
@@ -1,4 +1,7 @@
 use crate::serial::errors::Result;
+use std::io::{Read, Write};
+
+pub trait ReadWrite: Read + Write {}
 
 pub trait Connection {
     fn write_line(&mut self, line: &str) -> Result<()>;
