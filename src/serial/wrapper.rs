746790d6e8979779a44a35831e4b5bf02e901222
KMConner <KMConner@users.noreply.github.com>
2022-06-11 13:31:38 +0100

    Create serial port wrapper

---
+++ src/serial/wrapper.rs
@@ -0,0 +1,29 @@
+use super::traits::ReadWrite;
+use serialport::SerialPort;
+use std::io::{Read, Result, Write};
+
+pub struct Wrapper {
+    port: Box<dyn SerialPort>,
+}
+
+impl ReadWrite for Wrapper {}
+
+impl Read for Wrapper {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.port.read(buf)
+    }
+}
+
+impl Write for Wrapper {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        self.port.write(buf)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.port.flush()
+    }
+}
+
+pub fn new(conn: Box<dyn SerialPort>) -> Wrapper {
+    Wrapper { port: conn }
+}
