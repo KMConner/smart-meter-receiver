3ed245fb23451cfde173ece6a9b5d100149b439e
KMConner <KMConner@users.noreply.github.com>
2022-06-12 18:24:47 +0100

    Add client and mod

---
+++ src/wisun-module/client.rs
@@ -0,0 +1,13 @@
+use crate::serial::Connection;
+
+pub struct WiSunCLient {
+    serial_connection: Connection,
+}
+
+impl WiSunCLient {
+    fn new(serial_connection: Connection) -> Self {
+        WiSunCLient {
+            serial_connection: serial_connection,
+        }
+    }
+}
