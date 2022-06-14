6f8b5b375b0676ab05131c2c8e8fb2529e94a7e4
KMConner <KMConner@users.noreply.github.com>
2022-06-14 14:57:18 +0100

    Prepare test

--- src/wisun_module/client.rs
+++ src/wisun_module/client.rs
@@ -12,5 +12,20 @@ impl<T: Connection> WiSunCLient<T> {
         }
     }
 
-    fn ensure_echoback_off() -> Result<()> {}
+}
+#[cfg(test)]
+mod test {
+    use super::WiSunCLient;
+    use crate::wisun_module::mock::MockSerial;
+
+    fn new_client<F>(prepare_mock: F) -> WiSunCLient<MockSerial>
+    where
+        F: Fn(&mut MockSerial),
+    {
+        let mut mock = MockSerial::new();
+        prepare_mock(&mut mock);
+        WiSunCLient {
+            serial_connection: mock,
+        }
+    }
 }
