042a01c1c59afeac7860c3736befbe50d50183b3
KMConner <KMConner@users.noreply.github.com>
2022-08-06 00:52:27 +0900

    Implement line parser

--- src/parser/messages.rs
+++ src/parser/messages.rs
@@ -16,7 +16,54 @@ pub enum SerialMessage {
     Unknown(String),
 }
 
-#[cfg(test)]
-mod test{
+impl SerialMessage {
+    fn parse(data: &str) -> ParseResult<Self> {
+        if data == "OK" {
+            return ParseResult::Ok(SerialMessage::Ok);
+        }
 
+        if let Some(f) = data.strip_prefix("FAIL ") {
+            return ParseResult::Ok(SerialMessage::Fail(f.trim().to_string()));
+        }
+
+        match WiSunEvent::parse(data) {
+            ParseResult::Ok(ev) => ParseResult::Ok(SerialMessage::Event(ev)),
+            ParseResult::Err(_) => ParseResult::Err(data.to_string()),
+            ParseResult::More => ParseResult::More,
+            ParseResult::Empty => ParseResult::Empty,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_empty() {
+        assert_eq!(SerialMessage::parse(""), ParseResult::Empty);
+    }
+
+    #[test]
+    fn parse_ok() {
+        assert_eq!(SerialMessage::parse("OK"), ParseResult::Ok(SerialMessage::Ok));
+    }
+
+    #[test]
+    fn parse_fail() {
+        assert_eq!(SerialMessage::parse("FAIL 01"), ParseResult::Ok(SerialMessage::Fail(String::from("01"))));
+    }
+
+    #[test]
+    fn parse_event_more() {
+        assert_eq!(SerialMessage::parse("EPANDESC\n  Channel:2F\n  Channel Page:09"), ParseResult::More);
+    }
+
+    #[test]
+    fn parse_event_err() {
+        match SerialMessage::parse("EUNKNOWN") {
+            ParseResult::Err(_) => {}
+            _ => assert_eq!(1, 0),
+        };
+    }
 }
