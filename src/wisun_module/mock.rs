752ef282dc62cb546620ae7683efdc19fdd64b76
KMConner <KMConner@users.noreply.github.com>
2022-08-28 23:17:49 +0900

    Add parser mock

--- src/wisun_module/mock.rs
+++ src/wisun_module/mock.rs
@@ -5,11 +5,26 @@ use mockall::mock;
 use crate::serial::Connection;
 
 #[cfg(test)]
+use crate::parser::Parser;
+
+#[cfg(test)]
+use crate::parser::{ParseResult, SerialMessage};
+
+#[cfg(test)]
 mock! {
     pub Serial{}
 
     impl Connection for Serial {
         fn write_line(&mut self, line: &str) -> crate::serial::errors::Result<()>;
         fn read_line(&mut self) -> crate::serial::errors::Result<String>;
+    }
+}
+
+#[cfg(test)]
+mock! {
+    pub SerialParser{}
+
+    impl Parser for SerialParser{
+            fn add_line(&mut self, line: &str) -> ParseResult<SerialMessage>;
     }
 }
