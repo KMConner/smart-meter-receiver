63bef00542ac37625778335fd9651a641fd761ab
KMConner <KMConner@users.noreply.github.com>
2022-08-28 15:43:15 +0100

    Fix compile error

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use serial::Connection;
 use crate::wisun_module::WiSunClient;
 
 fn main() {
-    let mut conn = serial::new("/dev/ttyS0", 115200).unwrap();
+    let conn = serial::new("/dev/ttyS0", 115200).unwrap();
     let mut cli = WiSunClient::new(conn).unwrap();
-    let version = cli.get_version()?;
+    let version = cli.get_version().unwrap();
     println!("Version: {}", version);
 }
