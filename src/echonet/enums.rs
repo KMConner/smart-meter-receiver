00594e8135319b2bebb756a0cc04d08acacbd1c7
KMConner <KMConner@users.noreply.github.com>
2022-09-03 23:36:51 +0900

    Fix bugs echonet object conversion

--- src/echonet/enums.rs
+++ src/echonet/enums.rs
@@ -1,9 +1,8 @@
-use std::fmt::format;
 use num_enum::TryFromPrimitive;
 use crate::echonet::{Error, Result};
 
 #[repr(u64)]
-#[derive(Debug, PartialEq, TryFromPrimitive)]
+#[derive(Debug, PartialEq, TryFromPrimitive, Copy, Clone)]
 pub enum EchonetObject {
     SmartMeter = 0x028801,
     HemsController = 0x05FF01,
@@ -12,10 +11,10 @@ pub enum EchonetObject {
 impl Into<[u8; 3]> for EchonetObject {
     fn into(self) -> [u8; 3] {
         let u = self as u64;
-        let b2 = (u >> 16) as u8;
-        let b1 = (u >> 0) as u8;
-        let b0 = u as u8;
-        [b2, b1, b0]
+        let b0 = (u >> 16) as u8;
+        let b1 = (u >> 8) as u8;
+        let b2 = u as u8;
+        [b0, b1, b2]
     }
 }
 
@@ -23,12 +22,32 @@ impl TryFrom<[u8; 3]> for EchonetObject {
     type Error = Error;
 
     fn try_from(value: [u8; 3]) -> Result<Self> {
-        let num = ((value[2] as u64) << 16) + ((value[1] as u64) << 8) + value[0] as u64;
+        let num = ((value[0] as u64) << 16) + ((value[1] as u64) << 8) + value[2] as u64;
         match num.try_into() {
             Ok(e) => Ok(e),
             Err(_) => {
                 return Err(Error::InvalidEchonetObjectId(format!("{:?}", value)));
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::echonet::enums::EchonetObject;
+
+    #[test]
+    fn into_slice_test() {
+        let expected = hex::decode("028801").unwrap();
+        let actual: [u8; 3] = EchonetObject::SmartMeter.into();
+        let actual = actual.to_vec();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn from_slice_test() {
+        let expected = EchonetObject::SmartMeter;
+        let actual = [0x02, 0x88, 0x01].try_into().unwrap();
+        assert_eq!(expected, actual);
     }
 }
